2bee60f10200572fa745ec4fa506dcca12ed81fa
mengxianming <mengxianming@pset.suntec.net>
2014-09-29 10:58:23 +0800

    new bean

---
+++ HelloWorld/src/net/suntec/helloworld/bean/APIResult.java
@@ -0,0 +1,22 @@
+package net.suntec.helloworld.bean;
+
+public class APIResult {
+	private String code;
+	private Object data;
+	
+	public String getCode() {
+		return code;
+	}
+	public void setCode(String code) {
+		this.code = code;
+	}
+	public Object getData() {
+		return data;
+	}
+	public void setData(Object data) {
+		this.data = data;
+	}
+	
+	
+
+}
